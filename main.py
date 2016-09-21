672f756ea17e753446d9e385df35230923416af2
Mikhail Putilov <post.snowy@gmail.com>
2016-09-21 13:50:03 +0300

    add yield

--- main.py
+++ main.py
@@ -36,6 +36,22 @@ digits_to_lists = {
     "6": adverbs,
     "9": interjections
 }
+part_of_speech_to_lists = {
+    "verb": intransitive_verbs,
+    "noun": concrete_nouns,
+    "adverb": adverbs,
+    "adjective": adjectives
+}
+
+
+def get_rhyme(word, desired_part_of_speech):
+    return "whales"
+
+
+def get_part_of_speech_from_template(template):
+    digit = re.findall(r"\[(\w+)\]", template)[0]
+
+    return ""
 
 
 def gen_verses(random_pattern1: str, random_pattern2: str):
@@ -48,14 +64,25 @@ def gen_verses(random_pattern1: str, random_pattern2: str):
     split2 = [_map(x) for x in list(random_pattern2)]
     joined1 = "".join(split1)
     joined2 = "".join(split2)
+    # two random strings presented
+    last_word1 = extract_last_word_from_brackets(joined1)
+    final1 = replace_last_word_and_remove_brackets(joined1, last_word1)
 
-    last_word = re.findall(r"\[(\w+)\]", joined1)[0]
-    # print(last_word)
-    # return re.sub(r"\[\w+\]", "yo", joined1)
-    return joined1 + "\n" + joined2
+    rhyme_for_last_word1 = get_rhyme(last_word1, get_part_of_speech_from_template(random_pattern2))
+    final2 = replace_last_word_and_remove_brackets(joined2, rhyme_for_last_word1)
+    return final1, final2
+
+
+def replace_last_word_and_remove_brackets(joined1, last_word1):
+    return re.sub(r"\[(.+)\]", last_word1, joined1)
+
+
+def extract_last_word_from_brackets(joined1):
+    return re.findall(r"\[(\w+)\]", joined1)[0]
 
 
 def generate_poem():
+    # A A B B
     random_pattern1 = random.choice(patterns)
     random_pattern2 = random.choice(patterns)
     verses = gen_verses(random_pattern1, random_pattern2)
